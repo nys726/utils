da53a855d72d14d0b18a98591290486b7e31905c
ysna629 <ysna629@gmail.com>
2023-11-23 14:10:27 +0900

    Bump Up Version: test test test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="steeltools",
-    version="0.0.2",
+    version="0.0.3",
     description="for steel Project test",
     packages=find_packages(),
     install_requires=[
